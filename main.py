50547de40c7dd3f06ef2c21710807194ea84db03
ashxvegeta <ashutoshroy453@gmail.com>
2025-09-13 22:34:07 +0530

    Prompt templete

--- main.py
+++ main.py
@@ -13,3 +13,18 @@ llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.9)
 # Ask Gemini a question
 response = llm.invoke("Give me only ONE short name for an Indian restaurant, nothing else.")
 print(response.content)
+
+
+from langchain.prompts import PromptTemplate
+PromptTemplate_name = PromptTemplate(
+    input_variables=["cuisine"],
+    template="Give me only ONE short name for a {cuisine} restaurant, nothing else."
+)
+# prompt = PromptTemplate_name.format(cuisine="Indian")
+# print(prompt)
+# Fill template
+prompt = PromptTemplate_name.format(cuisine="Indian")
+
+# Use template with Gemini
+response = llm.invoke(prompt)
+print(response.content)
