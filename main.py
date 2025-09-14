e5a94e2d44398d5766b5b2ec4ca55be4e3a97168
ashxvegeta <ashutoshroy453@gmail.com>
2025-09-14 14:22:50 +0530

    Sequtial chain

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.6)
 
 # Ask Gemini a question
 response = llm.invoke("Give me only ONE short name for an Indian restaurant, nothing else.")
-print(response.content)
+
 
 
 from langchain.prompts import PromptTemplate
@@ -27,8 +27,18 @@ prompt = PromptTemplate_name.format(cuisine="Indian")
 
 # Use template with Gemini
 response = llm.invoke(prompt)
-print(response.content)
 
-chain = LLMChain(llm=llm, prompt=PromptTemplate_name)
-response = chain.run("mexican")
+
+name_chain = LLMChain(llm=llm, prompt=PromptTemplate_name)
+
+prompt_template_items = PromptTemplate(
+    input_variables=["restaurant_name"],
+    template="Give me a list of three {restaurant_name} that are used in Indian cooking. Respond in comma separated format, without any explanation."
+)
+
+food_chain = LLMChain(llm=llm, prompt=prompt_template_items)
+
+from langchain.chains import SimpleSequentialChain
+sequential_chain = SimpleSequentialChain(chains=[name_chain, food_chain])
+response = sequential_chain.run("Indian")
 print(response)
