06312a39f64482cceca267cdb1ffcfb239383cb3
ashxvegeta <ashutoshroy453@gmail.com>
2025-09-14 13:32:51 +0530

    LLM chain

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ os.environ["GOOGLE_API_KEY"] = gemini_api_key
 
 # Import Gemini LLM from LangChain
 from langchain_google_genai import ChatGoogleGenerativeAI
-
+from langchain.chains import LLMChain
 # Initialize Gemini model
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.9)
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.6)
 
 # Ask Gemini a question
 response = llm.invoke("Give me only ONE short name for an Indian restaurant, nothing else.")
@@ -28,3 +28,7 @@ prompt = PromptTemplate_name.format(cuisine="Indian")
 # Use template with Gemini
 response = llm.invoke(prompt)
 print(response.content)
+
+chain = LLMChain(llm=llm, prompt=PromptTemplate_name)
+response = chain.run("mexican")
+print(response)
