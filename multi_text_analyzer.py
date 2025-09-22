29ee5cb8dda5decc6bb6f725b6eaac87147c8c51
ashxvegeta <ashutoshroy453@gmail.com>
2025-09-22 23:16:17 +0530

    multi text analyzer

---
+++ multi_text_analyzer.py
@@ -0,0 +1,42 @@
+from secretkey import gemini_api_key
+import os
+os.environ["GOOGLE_API_KEY"] = gemini_api_key
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+import json
+
+# Step 1: Define the schema
+class Analysis(BaseModel):
+    summary: str = Field(description="One-sentence summary of the text")
+    keywords: list[str] = Field(description="Important keywords from the text")
+    sentiment: str = Field(description="Sentiment of the text: positive, negative, or neutral")
+    category: str = Field(description="General category of the text, e.g. programming, finance, sports, etc.")  
+
+parser = PydanticOutputParser(pydantic_object=Analysis)
+
+# Step 2: LLM
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash",temperature=0)
+
+# Step 3: Prompt
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant that analyzes text."),
+    ("user", "Analyze the following text:\n\n{text}\n\n{format_instructions}")
+])
+
+# step 4: Chain
+chain = prompt | llm | parser
+
+# multi text analysis
+texts = [
+    "Python and pandas are great for data science.",
+    "Messi scored two goals to win the football match.",
+    "The stock market dropped due to inflation concerns.",
+    "LangChain helps integrate LLMs into apps easily."
+]
+
+for i, t in enumerate(texts ,start=1):
+    response = chain.invoke({"text": t, "format_instructions": parser.get_format_instructions()})
+    print(f"\nText {i}: {t}")
+    print("Parsed Output:", response.dict())
